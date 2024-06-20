62a61820f3cbf410f398c4b517a37399c58c9adb
tubisaffo <tubisaffo@gmail.com>
2024-06-20 03:27:51 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed=None):
+        self.name = name
+        self.breed = breed
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value is None:
+            self._breed = None
+        elif value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            self._breed = None
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job=None):
+        self.name = name
+        self.job = job
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value is None or value in APPROVED_JOBS:
+            self._job = value
+        else:
+            self._job = None
+            print("Job must be in list of approved jobs.")
